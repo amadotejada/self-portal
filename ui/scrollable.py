d8dcd3e0d5fb73aa63ceeed604e42285151b7aed
Amado Tejada <tejada.amado@gmail.com>
2022-03-24 17:52:47 -0400

    Update scrollable.py

--- ui/scrollable.py
+++ ui/scrollable.py
@@ -1,4 +1,19 @@
+# Copyright 2021 Amado Tejada
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from PyQt5 import QtWidgets
+
 from .flowlayout import FlowLayout
 
 
@@ -14,7 +29,7 @@ class Scrollable(QtWidgets.QScrollArea):
         self.verticalScrollBar().setObjectName("scrollablescrollbar")
         self.widgetLayout = FlowLayout(self.scrollwidget)
 
-    def addApp(self, tabButton, bottomButton=None, height=50):
-        self.widgetLayout.addWidget(tabButton)
+    def addApp(self, app):
+        self.widgetLayout.addWidget(app)
         self.widgetLayout.setSpacing(5)
         self.setWidget(self.scrollwidget)
